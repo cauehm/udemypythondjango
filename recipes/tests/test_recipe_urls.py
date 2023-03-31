4abafd3c580cb41830ad2fbdba8b271cd5dc81a6
Cauê Hartwig Michelin <cauehm@gmail.com>
2023-03-31 01:02:29 -0300

    new file:   .coveragerc 	modified:   .vscode/settings.json 	modified:   base_templates/global/base.html 	modified:   pytest.ini 	new file:   recipes/templates/recipes/pages/search.html 	new file:   recipes/templates/recipes/partials/pagination.html 	modified:   recipes/templates/recipes/partials/search.html 	new file:   recipes/tests/__init__.py 	new file:   recipes/tests/test_recipe_base.py 	new file:   recipes/tests/test_recipe_category_view.py 	new file:   recipes/tests/test_recipe_detail_view.py 	new file:   recipes/tests/test_recipe_home_view.py 	new file:   recipes/tests/test_recipe_model_recipe.py 	new file:   recipes/tests/test_recipe_search_view.py 	renamed:    recipes/tests.py -> recipes/tests/test_recipe_urls.py 	modified:   recipes/urls.py 	modified:   recipes/views.py 	new file:   utils/pagination.py 	deleted:    utils/recipes/factory.py 	new file:   utils/test_pagination.py

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 
-# Create your tests here.
-
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
@@ -16,3 +14,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+    def test_recipe_search_url_is_correct(self):
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,36 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
+from utils.pagination import make_pagination_range
+
+# from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 6)
+    page_obj = paginator.get_page(current_page)
+    page_obj.paginator.get_page(current_page)
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+
+    )
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
@@ -27,10 +47,31 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True )
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
 
+    })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__contains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    )
+    # recipes = recipes.filter(is_published=True).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{ search_term }"',
+        'search_term': search_term,
+        'recipes': recipes,
     })
