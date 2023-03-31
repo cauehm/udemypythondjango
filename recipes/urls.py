4d00ff098f2caf58ff024a1ba5717f2cccfcaa5b
Cauê Hartwig Michelin <cauehm@gmail.com>
2023-03-31 01:01:19 -0300

    new file:   .vscode/settings.json

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from recipes.views import home
+
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),        
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
