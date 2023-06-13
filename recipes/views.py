7e2b76d051af3fe201026e47abf1f90aa4abf9ff
Cauê Hartwig Michelin <cauehm@gmail.com>
2023-06-13 20:38:28 -0300

    ssttuc

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,19 @@
 import os
 
 from django.db.models import Q
-from django.http import Http404
+from django.http import JsonResponse
+from django.http.response import Http404
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
 
-PER_PAGE = int(os.environ.get('PER_PAGE', 3))
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 class RecipeListViewBase(ListView):
     model = Recipe
     context_object_name = 'recipes'
-    paginate_by = None
     ordering = ['-id']
     template_name = 'recipes/pages/home.html'
 
@@ -33,7 +33,7 @@ class RecipeListViewBase(ListView):
         )
         ctx.update(
             {'recipes': page_obj, 'pagination_range': pagination_range}
-            )
+        )
         return ctx
 
 
@@ -41,26 +41,41 @@ class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
 
 
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+
+        return JsonResponse(
+            list(recipes_list),
+            safe=False
+        )
+
+
 class RecipeListViewCategory(RecipeListViewBase):
     template_name = 'recipes/pages/category.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        ctx.update({
+            'title': f'{ctx.get("recipes")[0].category.name} - Category | '
+        })
+
+        return ctx
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
-            category__id=self.kwargs.get('category_id'),
+            category__id=self.kwargs.get('category_id')
         )
 
         if not qs:
             raise Http404()
 
         return qs
-
-    def get_context_data(self, *args, **kwargs):
-        ctx = super().get_context_data(*args, **kwargs)
-        ctx.update({
-            'title': f'{ctx.get("recipes")[0].category.name} - Category | ',
-            })
-        return ctx
 
 
 class RecipeListViewSearch(RecipeListViewBase):
@@ -75,22 +90,22 @@ class RecipeListViewSearch(RecipeListViewBase):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
             Q(
-                Q(title__contains=search_term) |
+                Q(title__icontains=search_term) |
                 Q(description__icontains=search_term),
-            ),
-            is_published=True
+            )
         )
-
         return qs
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
         search_term = self.request.GET.get('q', '')
+
         ctx.update({
-            'page_title': f'Search for "{ search_term }"',
+            'page_title': f'Search for "{search_term}" |',
             'search_term': search_term,
             'additional_url_query': f'&q={search_term}',
-            })
+        })
+
         return ctx
 
 
@@ -101,14 +116,14 @@ class RecipeDetail(DetailView):
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
-        qs = qs.filter(            
-            is_published=True
-        )
+        qs = qs.filter(is_published=True)
         return qs
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+
         ctx.update({
-            'is_detail_page': True,
-            })
+            'is_detail_page': True
+        })
+
         return ctx
